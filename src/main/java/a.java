a7c216d32e7c337465430fdd910f3f88146722cf
xx <xxyyxxx@qq.com>
2023-04-09 13:04:02 +0800

    Revert "封神2"
    This reverts commit 7be1c9ee

--- src/main/java/a.java
+++ src/main/java/a.java
@@ -2,5 +2,7 @@ public class a {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git second");
+        System.out.println("hello git sssss");
+        System.out.println("hello git sssss1");
     }
 }
